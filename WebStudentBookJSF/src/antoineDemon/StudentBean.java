7b8a0669f2b8a43195d08d8060fd5785163621df
AugustinDlt <augustininscription@gmail.com>
2017-12-17 14:41:17 +0100

    one bean

--- WebStudentBookJSF/src/antoineDemon/StudentBean.java
+++ WebStudentBookJSF/src/antoineDemon/StudentBean.java
@@ -11,12 +11,30 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-@ManagedBean
+@ManagedBean(name="studentb")
 @SessionScoped
 
 public class StudentBean implements Serializable {
 	
 	private static final long serialVersionUID = 6081417964063918994L;
+	private Student student = new Student();
+	private int ids;
+	
+	public Student getStudent(){
+		return student;
+	}
+	
+	public void setStudent(Student student){
+		this.student=student;
+	}
+	
+	public int getIds(){
+		return ids;
+	}
+	
+	public void setIds(int id){
+		ids = id;
+	}
 	
 	public List<Student> getStudents() throws ClassNotFoundException, SQLException{
 		
@@ -61,4 +79,90 @@ public class StudentBean implements Serializable {
 		return students;
 	}
 
+	public void AddStudent() throws ClassNotFoundException, SQLException{
+		
+		Connection connect = null;
+
+		String url = "jdbc:mysql://localhost:3306/studentdb?useSSL=false";
+
+		String username = "root";
+		String password = "root";
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+
+			connect = DriverManager.getConnection(url, username, password);
+		}
+		catch (SQLException ex){
+			System.out.println("in exec");
+			System.out.println(ex.getMessage());
+		}
+		
+		String FName = student.getFirstName();
+		String LName = student.getLastName();
+		String Email = student.getEmail();
+		
+		PreparedStatement pstmt=connect.prepareStatement("INSERT INTO student (firstName, lastName, email) VALUES('" + FName + "','" + LName + "','" + Email + "')");
+		pstmt.executeUpdate();
+		
+		
+		pstmt.close();
+		connect.close();
+	}
+
+	public void deleteStudent() throws ClassNotFoundException, SQLException {
+		Connection connect = null;
+
+		String url = "jdbc:mysql://localhost:3306/studentdb?useSSL=false";
+
+		String username = "root";
+		String password = "root";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			connect = DriverManager.getConnection(url, username, password);
+		} catch (SQLException ex) {
+			System.out.println("in exec");
+			System.out.println(ex.getMessage());
+		}
+
+		int ID = ids;
+
+		PreparedStatement pstmt = connect.prepareStatement("DELETE FROM student WHERE id = " + ID);
+		pstmt.executeUpdate();
+
+		pstmt.close();
+		connect.close();
+	}
+	
+	public void editStudent() throws ClassNotFoundException, SQLException{
+		
+		Connection connect = null;
+
+		String url = "jdbc:mysql://localhost:3306/studentdb?useSSL=false";
+		
+		String username = "root";
+		String password = "root";
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+
+			connect = DriverManager.getConnection(url, username, password);
+		}
+		catch (SQLException ex){
+			System.out.println("in exec");
+			System.out.println(ex.getMessage());
+		}
+		
+		String FName = student.getFirstName();
+		String LName = student.getLastName();
+		String Email = student.getEmail();
+
+		PreparedStatement pstmt=connect.prepareStatement("UPDATE student SET firstname='" + FName + "', lastname='" + LName + "', email='" + Email + "' where id=" + ids);
+		pstmt.executeUpdate();
+
+		pstmt.close();
+		connect.close();
+	}
 }
